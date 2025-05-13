4a649fa6bd51e5749d84a47e2bd45fc85e4d348d
arFaisal043 <arfaisal463@gmail.com>
2025-05-14 01:16:53 +0600

    change in js file

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-let btn = document.getElementById("btn");
-let text = document.getElementById("text");
-// btn.innerText = "Hello World!";
-
-let changeText = ()=> {
-    text.innerText = "Hello World!";
+function sortFunction() {
+    let arr = [4 , 3 , 2 , 1];
+    n = arr.length;
+    for(let i = 0; i < n - 1; i++) {
+        for(let j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                [arr[j] , arr[j + 1]] = [arr[j + 1] , arr[j]];
+            }
+        }
+    }
+    return arr;
 }
 
+console.log(sortFunction());
