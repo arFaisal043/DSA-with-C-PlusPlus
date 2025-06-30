5a3137fb743fb5fce862730926ae500cef11341e
arFaisal043 <arfaisal463@gmail.com>
2025-06-30 14:55:59 +0600

    changes on stl.cpp

--- stl.cpp
+++ stl.cpp
@@ -1,16 +1,66 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
 int main() {
-    vector<int> vec = {12 , 31 ,35 , 8 , 32 , 17};
 
-    sort(vec.begin() , vec.end());
-    
-    for(int val : vec) {
-        cout << val << " ";
+    pair<int , pair<int , string>> p = {1 , {2 , "Faisal"}};
+    cout << p.first << endl;
+    cout << p.second.first << endl;
+    cout << p.second.second << endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+Iterators in Vector:
+
+(for forward iteration) =>
+
+int main() {
+
+    vector<int> vec = {1, 2, 3, 4, 5};
+    vector<int>:: iterator itr; // Declared a iterator
+    for(itr = vec.begin(); itr != vec.end(); itr++) {
+        cout << *(itr) << " ";
     }
 
     return 0;
 }
+
+(for Backward iteration) =>
+
+int main() {
+
+    vector<int> vec = {1, 2, 3, 4, 5};
+    vector<int>:: reverse_iterator itr; // Declared a iterator for backward
+    for(itr = vec.rbegin(); itr != vec.rend(); itr++) {
+        cout << *(itr) << " ";
+    }
+
+    return 0;
+}
+
+auto for replace to the iterations declared:
+    vector<int> vec = {1, 2, 3, 4, 5};
+    for(auto itr = vec.rbegin(); itr != vec.rend(); itr++) {
+        cout << *(itr) << " ";
+    }
+
+*/
